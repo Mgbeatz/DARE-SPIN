5d42c6b79ff56e51a3bc1cfa92c3e049e7a011eb
Mg beatz Icrowdx <127049273+Mgbeatz@users.noreply.github.com>
2025-02-01 09:07:11 -0800

    Add files via upload

---
+++ DARE SPIN/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1738428757,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/rectangle-sheet0.png",
+		"images/roundedrectangle-sheet0.png",
+		"images/bgd-sheet0.png",
+		"images/slot-sheet0.png",
+		"images/slot-sheet1.png",
+		"images/mouses-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"orhvs - ramutsheli.pdf",
+		"penumbra.png"
+	]
+}
